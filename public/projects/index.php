210839fcf83aa63ebd8185a099d719ad6c8f7ae9
Yannoel_MARTIN <yannoel.martin@hotmail.fr>
2023-08-03 14:14:39 +0200

    maj gabarit

--- public/projects/index.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-include_once __DIR__ . '/../../system/functions.php';
-
-create_content_redirection_page();
-
-echo "Ici se trouvera les pages personnalisées par projet (téléchargement)<br>Fonctionnalité a venir";
